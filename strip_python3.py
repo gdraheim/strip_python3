3f9fe3809a0a14c87a6c8002770fb78f34aaabeb
Guido Draheim <guidod@gmx.de>
2025-03-07 02:12:27 +0100

    v0.5.1095

--- strip_python3.py
+++ strip_python3.py
@@ -4,7 +4,7 @@
 
 __copyright__ = "(C) 2025 Guido Draheim, licensed under MIT License"
 __author__ = "Guido U. Draheim"
-__version__ = "0.4.1095"
+__version__ = "0.5.1095"
 
 from typing import List, Dict, Optional, Union, Tuple, cast
 import sys
