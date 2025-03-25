0e9654f8f6468cc43cc5abdad7d47046e6cac1a7
Guido Draheim <guidod@gmx.de>
2025-03-25 18:13:02 +0100

    v1.0.1122

--- src/strip_python3.py
+++ src/strip_python3.py
@@ -6,7 +6,7 @@
 
 __copyright__ = "(C) 2025 Guido Draheim, licensed under MIT License"
 __author__ = "Guido U. Draheim"
-__version__ = "1.0.1121"
+__version__ = "1.0.1122"
 
 from typing import Set, List, Dict, Optional, Union, Tuple, cast, NamedTuple, TypeVar, Deque, Iterable
 import sys
