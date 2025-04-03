eb9bd1ab70360bbe6711ddef6e801ffef3163bdf
Guido Draheim <guidod@gmx.de>
2025-04-03 13:35:13 +0200

    v1.1.1134

--- tests/unittests1.py
+++ tests/unittests1.py
@@ -5,7 +5,7 @@
 
 __copyright__ = "(C) 2025 Guido Draheim, licensed under MIT License"
 __author__ = "Guido U. Draheim"
-__version__ = "1.1.1132"
+__version__ = "1.1.1134"
 
 from typing import cast
 import sys
