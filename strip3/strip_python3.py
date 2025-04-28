3678eabe88a7af82cd265dc0ffd326555676d88a
Guido Draheim <guidod@gmx.de>
2025-04-28 02:31:22 +0200

    strip_qtoml_decoder.py -> qtoml_decoder.py

--- strip3/strip_python3.py
+++ strip3/strip_python3.py
@@ -24,7 +24,7 @@ else:  # pragma: nocover
         import tomli as tomllib # type: ignore[no-redef,import-untyped]
     except ImportError:
         try:
-            import strip_qtoml_decoder as tomllib # type: ignore[no-redef,import-untyped]
+            import qtoml_decoder as tomllib # type: ignore[no-redef,import-untyped]
         except ImportError:
             tomllib = None # type: ignore[assignment]
 DEBUG_TOML = logging.DEBUG
