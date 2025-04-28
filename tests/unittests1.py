92762e5e7c963f21a953b32a97eeed39481d266a
Guido Draheim <guidod@gmx.de>
2025-04-28 03:09:43 +0200

    fix import prio

--- tests/unittests1.py
+++ tests/unittests1.py
@@ -17,7 +17,7 @@ import ast
 
 logg = logging.getLogger(os.path.basename(__file__))
 
-sys.path.append(os.curdir)
+sys.path = [os.path.abspath(os.curdir)] + sys.path
 from strip3 import strip_python3 as app # pylint: disable=wrong-import-position,import-error,no-name-in-module
 
 TODO = 0
